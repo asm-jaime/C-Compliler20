a5141d5e98e65841729cc74d92c8ec118b83bba1
asm-jaime <asm.jaime@gmail.com>
2023-06-10 12:54:54 +1000

    add simple test in test

---
+++ OnlineCompiler/Server/Handlers/CodeCompileChecker.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineCompiler.Server.Handlers;
+
+public static class CodeCompileChecker<T>
+{
+    public static object? Check(Stack<int> stack, Type constructedType, object? stackInstance)
+    {
+        return false;
+    }
+}
