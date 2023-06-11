1da3987637b1809b0bcc17b714e0e027a582a23c
asm-jaime <asm.jaime@gmail.com>
2023-06-11 10:17:27 +1000

    add full example set for stack

--- OnlineCompiler/Server/Handlers/CodeCompileChecker.cs
+++ OnlineCompiler/Server/Handlers/CodeCompileChecker.cs
@@ -2,21 +2,25 @@
 
 public static class CodeCompileChecker<T>
 {
-
-    public static bool CheckPushPushPop(Stack<T> stack, string code, T item)
+    private static (Type, object) GetInstance(string code, string strType)
     {
-        var stackType = DynamicClassCreator.CreateClassFromCode(code, "Stack");
-        Type constructedType = stackType.MakeGenericType(typeof(T));
-        var stackInstance = Activator.CreateInstance(constructedType);
+        var type = DynamicClassCreator.CreateClassFromCode(code, strType);
+        Type constructedType = type.MakeGenericType(typeof(T));
+        var instance = Activator.CreateInstance(constructedType);
+        return (constructedType, instance);
+    }
 
-        stack.Push(item);
-        stack.Push(item);
-        constructedType.GetMethod("Push").Invoke(stackInstance, new object[] {item});
-        constructedType.GetMethod("Push").Invoke(stackInstance, new object[] {item});
+    public static bool CheckStack(string code, T item)
+    {
+        Stack<T> stack = new Stack<T>();
+        var (type, instance) = GetInstance(code, "Stack");
 
-        var origin = stack.Pop();
-        var constructed = (T)constructedType.GetMethod("Pop").Invoke(stackInstance, null);
-
-        return origin.Equals(constructed);
+        return CheckStack<T>.CheckPush(stack, type, instance, item)
+            && CheckStack<T>.CheckPeek(stack, type, instance, item)
+            && CheckStack<T>.CheckTryPeek(stack, type, instance, item)
+            && CheckStack<T>.CheckClear(stack, type, instance, item)
+            && CheckStack<T>.CheckContains(stack, type, instance, item)
+            && CheckStack<T>.CheckToArray(stack, type, instance, item);
     }
 }
+
